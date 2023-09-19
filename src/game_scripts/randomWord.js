7bc74e1e72e80422577d4aab8461c9e0c2e77855
BomScoob <sorawitkraukham@gmail.com>
2023-09-19 15:16:15 +0700

    update randomAllplayer

--- src/game_scripts/randomWord.js
+++ src/game_scripts/randomWord.js
@@ -1,53 +1,111 @@
 const mapPlayer = new Map();
+const debugMode = true;
 const player1 = {
-    username: "p1",
-    word: ["good1", "bad1"]
-}
+  username: "p1",
+  word: ["good1", "bad1"],
+};
 const player2 = {
-    username: "p2",
-    word: ["good2", "bad2"]
-}
+  username: "p2",
+  word: ["good2", "bad2"],
+};
 const player3 = {
-    username: "p3",
-    word: ["good3", "bad3"]
-}
-mapPlayer.set(0, player1)
-mapPlayer.set(1, player2)
-mapPlayer.set(2, player3)
+  username: "p3",
+  word: ["good3", "bad3"],
+};
+mapPlayer.set(0, player1);
+mapPlayer.set(1, player2);
+mapPlayer.set(2, player3);
 
-let goodWord = [];
-let badWord = [];
-
-//for 1 times called .length
-const countAllplayer = mapPlayer.size;
-for (let i = 0; i< countAllplayer; i++){
-    const playerTemp = mapPlayer.get(i);
-    console.log(playerTemp.word)
-    goodWord.push(playerTemp.word[0])
-    badWord.push(playerTemp.word[1])
+let listOfGoodWord = [];
+let listOfBadWord = [];
+//debug
+function logDebug(massage) {
+  if (debugMode === true) {
+    console.log(massage);
+  }
 }
 
-function randomWordAllPlayer(allPlayer, goodWord, badWord){
-    
+function getAllWord(mapOfPlayer) {
+  const countAllplayer = mapOfPlayer.size;
+  for (let i = 0; i < countAllplayer; i++) {
+    const player = mapOfPlayer.get(i);
+    logDebug("get player from map : " + player);
+    listOfGoodWord.push(player.word[0]);
+    logDebug(`Good word adding to array : ${listOfGoodWord}`);
+    listOfBadWord.push(player.word[1]);
+    logDebug(`Good word adding to array : ${listOfGoodWord}`);
+  }
 }
+getAllWord(mapPlayer)
+logDebug(listOfGoodWord)
+logDebug(listOfBadWord)
+function getRandomIndex(arrayOfWord) {
+  return Math.floor(Math.random() * arrayOfWord.length);
+}
+function randomWordAllPlayer(mapOfPlayer, arrOfGoodWord, arrOfBadWord) {
+  for(let player of mapOfPlayer.values()){
+    const tempGood = arrOfGoodWord.filter((word) => !player.word.includes(word));
+    logDebug(tempGood, 'temp good');
+    const tempBad = arrOfBadWord.filter((word) => !player.word.includes(word));
+    logDebug(tempBad, 'temp bad');
 
-function randomWord(player , arrayOfWord){
-    const randomIndex = Math.floor(Math.random() * arrayOfWord.length)
-    const wordRandom = arrayOfWord[randomIndex]
-    if(player.word.includes(wordRandom)){
-        randomWord(player, arrayOfWord)
-    }else {
-        if(!player.hasOwnProperty("randomWord")){
-            player.randomword = [wordRandom];
-        }else {
-            player.randomWord.push(wordRandom);
-        }
-        // remove element in array
-        arrayOfWord.splice(randomIndex, 1)
+    const indexGood = getRandomIndex(tempGood);
+    const indexBad = getRandomIndex(tempBad);
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (!player.hasOwnProperty("randomWord")) {
+      player.randomWord = new Array();
+      logDebug("create new property array")
     }
+    player.randomWord = [arrOfGoodWord[indexGood], arrOfBadWord[indexBad]]
+    arrOfGoodWord.splice(indexGood, 1);
+    arrOfBadWord.splice(indexBad, 1);
+  }
 }
+randomWordAllPlayer(mapPlayer, listOfGoodWord, listOfBadWord)
+logDebug(mapPlayer)
 
-// randomWord(player1, goodWord)
-// console.log(player1)
+// function randomWord(player, arrayOfWord) {
+//   if (!player && arrayOfWord.length === 0) return;
+//   const randomIndex = Math.floor(Math.random() * arrayOfWord.length);
+//   logDebug(`random index : ${randomIndex}`);
+//   const wordRandom = arrayOfWord[randomIndex];
+//   logDebug(`random word from array ${arrayOfWord} is :  ${wordRandom}`);
+//   if (player.word.includes(wordRandom)) {
+//     logDebug("player get own word. random again");
+//     randomWord(player, arrayOfWord);
+//   } else {
+//     if (player["getRandom"] === undefined) {
+//       player["getRandom"] = new Array(wordRandom);
+//       logDebug(`create new property and get randomword: ${player.getRandom}`);
+//     } else {
+//       player.getRandom.push(wordRandom);
+//       logDebug("push new element to randomWord property : " + player.getRandom);
+//     }
+//     // remove element in array
+//     arrayOfWord.splice(randomIndex, 1);
+//     logDebug("remove used element.");
+//   }
+// }
+
+// function randomWordAllPlayer(mapOfPlayer, arrOfGoodWord, arrOfBadWord) {
+//   if (!mapOfPlayer) return;
+//   //loop and randomword
+//   for (let i = mapOfPlayer.size - 1; i >= 0; i--) {
+//     let player = mapOfPlayer.get(i);
+//     randomWord(player, arrOfGoodWord);
+//     randomWord(player, arrOfBadWord);
+//     if(arrOfBadWord.length === 1 || arrOfGoodWord.length === 1){
+//       player.getRandom = new Array(arrOfGoodWord[0], arrOfBadWord[0])
+//     }
+//   }
+// }
+// randomWordAllPlayer(mapPlayer, goodWord, badWord);
+// console.log(mapPlayer);
+// console.log(mapPlayer.get(1))
+// const testPlayer = mapPlayer.get(1);
+// randomWord(testPlayer, goodWord);
+// randomWord(testPlayer, badWord);
+// console.log(testPlayer);
 // console.log(goodWord)
 // console.log(badWord)
